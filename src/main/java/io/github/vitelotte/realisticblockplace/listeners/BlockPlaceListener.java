0b448af45b907352df9367acf2ae0cf458af5b9a
sweetenpotato <69999133+go9um4@users.noreply.github.com>
2024-01-01 01:42:53 +0900

    init: Initialize BlockPlace Listener

--- src/main/java/io/github/vitelotte/realisticblockplace/listeners/BlockPlaceListener.java
+++ src/main/java/io/github/vitelotte/realisticblockplace/listeners/BlockPlaceListener.java
@@ -1,2 +1,20 @@
-package io.github.vitelotte.realisticblockplace.listeners;public class BlockPlaceListener {
+package io.github.vitelotte.realisticblockplace.listeners;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.Plugin;
+
+public class BlockPlaceListener implements Listener {
+    private Plugin plugin;
+    public BlockPlaceListener(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+
+    }
 }
