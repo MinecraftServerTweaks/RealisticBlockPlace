36ec2585f610dd388ca7d9d0a436bcedf5367502
sweetenpotato <69999133+go9um4@users.noreply.github.com>
2024-01-01 02:08:06 +0900

    feat: Chat Prefix and Supporting Block

--- src/main/java/io/github/vitelotte/realisticblockplace/RealisticBlockPlace.java
+++ src/main/java/io/github/vitelotte/realisticblockplace/RealisticBlockPlace.java
@@ -1,11 +1,20 @@
 package io.github.vitelotte.realisticblockplace;
 
+import io.github.vitelotte.realisticblockplace.listeners.BlockPlaceListener;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RealisticBlockPlace extends JavaPlugin {
+    public TextComponent chatPrefixComponent = Component.text("Server", NamedTextColor.AQUA);
+    public Material supportingBlock = Material.BEDROCK;
 
     @Override
     public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new BlockPlaceListener(this), this);
     }
 
     @Override
