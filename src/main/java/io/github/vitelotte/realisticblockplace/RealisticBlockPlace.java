51b5424ab467295bca68b9cc2e7c00ff04b09166
sweetenpotato <69999133+go9um4@users.noreply.github.com>
2024-01-01 01:41:30 +0900

    init: Initialize Project

---
+++ src/main/java/io/github/vitelotte/realisticblockplace/RealisticBlockPlace.java
@@ -0,0 +1,15 @@
+package io.github.vitelotte.realisticblockplace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RealisticBlockPlace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
