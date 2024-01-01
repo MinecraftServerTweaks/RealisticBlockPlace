2a989bbe5c6776b0c840d3ac83a1c2d21f04564b
sweetenpotato <69999133+go9um4@users.noreply.github.com>
2024-01-01 12:48:30 +0900

    refactor: apply separated checking placeable methods

--- src/main/java/io/github/vitelotte/realisticblockplace/listeners/BlockPlaceListener.java
+++ src/main/java/io/github/vitelotte/realisticblockplace/listeners/BlockPlaceListener.java
@@ -1,6 +1,7 @@
 package io.github.vitelotte.realisticblockplace.listeners;
 
 import io.github.vitelotte.realisticblockplace.RealisticBlockPlace;
+import io.github.vitelotte.realisticblockplace.utils.PlaceUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
@@ -25,7 +26,16 @@ public class BlockPlaceListener implements Listener {
         player.sendMessage(event.getBlockPlaced().getLocation().toString());
         if (!player.isOp()) {
             Block block = event.getBlock();
-            if (!this.hasSupportingBlock(block.getLocation()) && !block.getBlockData().getMaterial().equals(plugin.supportingBlock)) {
+            if (PlaceUtil.isSupportingBlock(block)) {
+                player.sendMessage(
+                        Component.text("[")
+                                .append(plugin.chatPrefixComponent)
+                                .append(Component.text("] "))
+                                .append(Component.text("부유석", NamedTextColor.LIGHT_PURPLE))
+                                .append(Component.text("에서 나오는 신비로운 기운이 자신의 위에 있는 블록들을 지지합니다!"))
+                );
+            }
+            else if (!PlaceUtil.hasSupportingBlock(block.getLocation())) {
                 event.setCancelled(true);
                 player.sendMessage(
                         Component.text("[")
@@ -35,24 +45,7 @@ public class BlockPlaceListener implements Listener {
                                 .append(Component.text("을 제외한 블럭을 설치할 수 없습니다!"))
                 );
             }
-            else if (block.getBlockData().getMaterial().equals(plugin.supportingBlock)) {
-                player.sendMessage(
-                        Component.text("[")
-                                .append(plugin.chatPrefixComponent)
-                                .append(Component.text("] "))
-                                .append(Component.text("부유석", NamedTextColor.LIGHT_PURPLE))
-                                .append(Component.text("에서 나오는 신비로운 기운이 자신의 위에 있는 블록들을 지지합니다!"))
-                );
-            }
         }
     }
 
-    private Boolean hasSupportingBlock(Location currentLocation) {
-        for (Double y = -64.0; y < currentLocation.getWorld().getMaxHeight(); y++) {
-            currentLocation.setY(y);
-            Material currentMaterial = currentLocation.getBlock().getBlockData().getMaterial();
-            if (!currentMaterial.equals(Material.AIR)) return currentMaterial.equals(plugin.supportingBlock);
-        }
-        return false;
-    }
 }
